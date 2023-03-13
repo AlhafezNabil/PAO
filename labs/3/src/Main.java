0b02bbdf93d2d06c57bd8f8ee33b72cf86143a92
AlhafezNabil <nabil.alhafez@unibuc.ro>
2023-03-13 17:16:54 +0200

    Initialize the project

--- labs/3/src/Main.java
+++ labs/3/src/Main.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        char c = 't';
-        Character cc = c;
+//        char c = 't';
+//        Character cc = c;
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter you name");
+        String name = scanner.nextLine();
 
+        System.out.println("Enter your age");
+        int age = scanner.nextInt();
+        System.out.println("Numbers are: ");
 
+        for (int i = (age % 2 == 1) ? 1 : 0; i <= age; i += 2) {
+            System.out.print(i);
+            System.out.print(' ');
+        }
+        scanner.close();
     }
 }
+
