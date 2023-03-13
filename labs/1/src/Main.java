00de02b89d4e98f7182ea1ceab9920728c9ca81e
AlhafezNabil <nabil.alhafez@unibuc.ro>
2023-03-13 16:36:17 +0200

    Lab3

---
+++ labs/1/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        int arr [] ={12,23,44,56,78};
+        int [] v = {13},w ={1,2};
+        int a[] = new int[10];
+
+//        System.out.println(v);
+//        System.out.println(w);
+
+        //Printing array using for-each loop
+        for(int i : v ){
+            System.out.println(i);
+        }
+
+        for(int i : w ){
+            System.out.println(i);
+        }
+
+
+    }
+}
---
+++ labs/3/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        char c = 't';
+        Character cc = c;
+
+
+
+    }
+}
