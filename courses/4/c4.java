38af02f05471a2b24a93fb2a0252f0f4323ad8ec
AlhafezNabil <nabil.alhafez@unibuc.ro>
2023-03-16 12:56:58 +0200

    Add curs4

---
+++ courses/4/c4.java
@@ -0,0 +1,15 @@
+
+
+
+
+// Case -- proprietarul -- aggrigare (weak association)
+// Case -- camera       -- compozite (strong association)
+
+//                                    state                                 used for
+// siruri de ==> 1- string          : immutable                             if we don't have much changes
+//               2- string builder  : is mutable and not thread-safe
+//               3- string buffer   : is mutable and thread-safe            parsing files
+
+// string s = "Test" getting one char s.charAr(0);
+public class c4 {
+}
